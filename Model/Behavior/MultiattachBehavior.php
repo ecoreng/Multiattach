3a8b860eda3a52444e760dac2ffb53254725a8a6
ecoreng <ecor.rck@hotmail.com>
2013-11-05 00:35:20 -0800

    fixed sort order

--- Model/Behavior/MultiattachBehavior.php
+++ Model/Behavior/MultiattachBehavior.php
@@ -50,7 +50,7 @@ class MultiattachBehavior extends ModelBehavior {
 				);
 				$model->Multiattach->recursive = -1;
 				if (isset($results[$i][$model->alias]['id'])) {
-					$results[$i]['Multiattach'] = $model->Multiattach->find('all', array('order' => array('Multiattach.id DESC'), 'conditions' => array('Multiattach.node_id' => $results[$i][$model->alias]['id'])));
+					$results[$i]['Multiattach'] = $model->Multiattach->find('all', array('order' => array('Multiattach.order ASC, Multiattach.id DESC'), 'conditions' => array('Multiattach.node_id' => $results[$i][$model->alias]['id'])));
 				}
 			}
 		}
